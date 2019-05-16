8c0ee76885a39c8796081ee18e08fbb9406fe10a
Mayank Gupta <mayank.gupta.6.88@gmail.com>
2019-05-16 18:09:36 +0530

    Changes

---
+++ JavaScriptPatterns/SingletonPattern.js
@@ -0,0 +1,38 @@
+
+
+var employeeData = (function(name, age, designation) {
+    var name = name;
+    var age = age;
+    var designation = designation;
+
+    (function() {
+        throw "Some Error"
+    })();
+
+    return {
+        empName: name,
+        empAge: age,
+        empDesignation: designation
+    }
+})("Mayank", 10, "Developer")
+
+
+console.log(employeeData.age);
+
+// ** Working with Named and Anonymous Function (Benefits)
+
+var userInfo = {};
+
+var singletonData = function() {
+    if(userInfo == {}) {
+
+        fetch("someUrl").then((data) => {
+            userInfo = data;
+        })
+    } else return userInfo;
+}
+
+var initialData = singletonData();
+var refreshedData = singletonData();
+
+
