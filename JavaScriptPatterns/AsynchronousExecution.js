b1f61d4747ca73594240df76e3c2cec2d6a64fd0
Mayank Gupta <mayank.gupta.6.88@gmail.com>
2019-05-17 19:40:18 +0530

    Higher Order Function

---
+++ JavaScriptPatterns/AsynchronousExecution.js
@@ -0,0 +1,25 @@
+var name = "Mayank";
+var age = 10;
+var designation = "Developer";
+
+for(let i=0; i< 10; i++) {
+    console.log(i);
+}
+
+setTimeout(function timeout() {
+    console.log("Click the button!");
+}, 5000);
+
+
+setTimeout(function timeout() {
+    console.log("Click the button!");
+}, 5000);
+
+setTimeout(function timeout() {
+    console.log("Click the button!");
+}, 5000);
+
+setTimeout(function timeout() {
+    console.log("Click the button!");
+}, 5000);
+
