a03d625c2bfd5e02f29147d7eb23adf66adf10f3
Mayank Gupta <mayank.gupta.6.88@gmail.com>
2019-05-20 19:30:42 +0530

    Initial Application

--- employee-management/src/index.js
+++ employee-management/src/index.js
@@ -1,4 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<div className="my-name">Hello World</div>, document.getElementsByClassName("another-div")[0]);
+ReactDOM.render(<div className="my-name">Hello World.. This is Test..</div>, document.getElementsByClassName("another-div")[0]);
+
+ReactDOM.render(<div className="my-name">Hello World.. Another Component...</div>, document.getElementById("root"));
+
+ReactDOM.render(<div className="my-name">Hello World.. Another Component... Again...</div>, document.getElementsByTagName("b")[0]);
+
